8bd2f9a29b8ecb80eb2b264203d4a1a428bb3548
Phil Harrison <philmichaelharrison@gmail.com>
2019-01-08 17:32:56 +0000

    Monday work edit

--- src/main/scala/Monday.scala
+++ src/main/scala/Monday.scala
@@ -2,103 +2,67 @@ object Monday extends App {
   //tasks completed up to iteration 4
   println("Hello world")
 
-  var a = "hello world"
-  println(a)
+  val message = "hello world"
+  println(message)
 
-  def printWord(a: String) {
-    println(a)
-  }
+  def printWord(message: String)= println(message)
 
   printWord("Hello world")
 
-  def helloWorld2(): String = {
-    val a = "hello world"
-    a
-  }
+  def helloWorld2():String = "hello world"
 
   println(helloWorld2())
 
-
-  def strings(a: String, b: Int): String = {
-    val c = a.substring(a.length - b, a.length)
-    c
-  }
+  def strings(message: String, b: Int): String = message.substring(message.length - b, message.length)
 
   println(strings("hello", 3))
 
-  def strings2(a: String, b: String, c: Char, d: Char): String = {
-    val combination = a + b
+  def strings2(firstString: String, secondString: String, firstChar: Char, secondChar: Char): String = {
+    val combination = firstString + secondString
     var myreturn = ""
 
     for (i <- 0 until combination.length) {
-      if (combination.substring(i, i + 1) == c.toString) {
-        myreturn = myreturn + d
-
-      }
-      else {
-        myreturn += combination.substring(i, i + 1)
-      }
-
+      if (combination.substring(i, i + 1) == firstChar.toString) myreturn = myreturn + secondChar
+      else myreturn += combination.substring(i, i + 1)
     }
     myreturn
   }
 
   println(strings2("Ha", "llo", 'a', 'e'))
 
-  def conditionals(a: Int, b: Int, bool: Boolean): Int = {
-    if (a == 0 || b == 0) {
-      if (b > a) {
-        b
-      }
-      else {
-        a
-      }
+  def conditionals(firstInt: Int, secondInt: Int, bool: Boolean): Int = {
+    if (firstInt == 0 || secondInt == 0){
+      if (secondInt > firstInt) secondInt
+      else firstInt
     }
-    else if (bool) {
-      a + b
-    }
-    else {
-      a * b
-    }
+    else if (bool) firstInt + secondInt
+    else firstInt * secondInt
   }
 
   println(conditionals(0, 3, 3 != 3))
 
-  def iteration(a: String, b: Int) {
-    for (i <- 1 to b) {
-      println(a)
-    }
+  def iteration(myString: String, loops: Int) {
+    for (i <- 1 to loops) println(myString)
   }
 
   iteration("H", 4)
 
 
-  def iteration2(a: String, b: Int) {
-    for (i <- 1 to b) {
-      for (i <- 1 to b) {
-        print(a)
-      }
-      print("\n")
-    }
+  def iteration2(myString: String, loops: Int) {
+    for (i <- 1 to loops) println(myString*loops)
   }
 
   iteration2("H", 4)
 
-  def iteration3(a: String, b: String, c: Int) {
-    for (i <- 1 to c) {
-      if (i % 3 == 0 && i % 5 == 0) {
-        print(a + b)
+  def iteration3(firstWord: String, secondWord: String, num1: Int) {
+    for (i <- 1 to num1) {
+      num1 match{
+        case fb if i % 3 == 0 && i % 5 == 0 => print(firstWord + secondWord)
+        case f if i % 3 == 0 => print(firstWord)
+        case b if i % 5 ==0 => print(secondWord)
+        case _ => print(i)
       }
-      else if (i % 3 == 0) {
-        print(a)
-      }
-      else if (i % 5 == 0) {
-        print(b)
-      }
-      else {
-        print(i)
-      }
-      if (i != c) {
+      if (i != num1) {
         print(",")
       }
     }
@@ -107,51 +71,41 @@ object Monday extends App {
   iteration3("Fizz", "Buzz", 15)
   println("")
 
-  def recursion(a: String, b: Int) {
+  def recursion(myString: String, b: Int) {
     if (b > 0) {
-      println(a)
-      recursion(a, b - 1)
+      println(myString)
+      recursion(myString, b - 1)
     }
   }
 
   recursion("H", 4)
 
-  def recursion2(a: String, b: Int) {
+  def recursion2(myString: String, b: Int) {
 
     val columns = b
     if (b > 0) {
-      println(a*b)
-      recursion2(a, b - 1)
+      println(myString*b)
+      recursion2(myString, b - 1)
 
-    }
-    else {
     }
   }
 
   recursion2("H", 4) //needs work, reduces the amount of times it prints the string with each turn
 
-  def recursion3(a: String, b: String, c: Int): String = {
+  def recursion3(a: String, b: String, num1: Int):String = {
     var output = ""
-    logicChecks(a, b, c)
+    logicChecks(a, b, num1)
 
-    def logicChecks(a: String, b: String, c: Int) {
-      if (c > 0) {
-        if (c % 3 == 0 && c % 5 == 0) {
-          output = a + b + output
+    def logicChecks(firstWord: String, secondWord: String, num1: Int):Unit={
+      if (num1 > 0) {
+        num1 match{
+          case a if num1 % 3 == 0 && num1 % 5 == 0 => output = firstWord + secondWord + output
+          case b if num1 % 3 == 0 => output = firstWord + output
+          case c if num1 % 5 == 0 => output = secondWord + output
+          case _ => output = num1.toString + output
         }
-        else if (c % 3 == 0) {
-          output = a + output
-        }
-        else if (c % 5 == 0) {
-          output = b + output
-        }
-        else {
-          output = c.toString + output
-        }
-        if(c!=1){
-          output = "," + output
-        }
-        logicChecks(a, b, c - 1)
+        if(num1!=1) output = "," + output
+        logicChecks(firstWord, secondWord, num1 - 1)
       }
     }
     output
