15843486ad3ac14d46cb50a0c4c53dc6578ec0e7
Phil Harrison <philmichaelharrison@gmail.com>
2019-01-10 15:37:40 +0000

    Thursday Work

--- src/main/scala/Keyboard.scala
+++ src/main/scala/Keyboard.scala
@@ -1,21 +1,25 @@
+import scala.io.Source
+import scala.io.StdIn._
+import scala.collection.mutable.ArrayBuffer
+
 object Keyboard extends App {
-  import scala.io.Source
-  import scala.io.StdIn._
-  println("Enter number of character strings to read")
-  val wordAmount = readInt
-  var stringsArray = new Array[String](wordAmount)
-  println("Type that many strings")
-  for (i <- 0 until wordAmount)stringsArray(i) = readLine
+
+  println("Enter number of broken keyboards")
+  val stringsArray = new ArrayBuffer[String]()
+
+  for (i <- 0 until readInt){
+    println(s"Type the available keys for keyboard number $i")
+    stringsArray.append(readLine)
+  }
 
   val dictionary = Source.fromFile("C:\\Users\\Admin\\Desktop\\Scala\\Material\\enable1.txt").getLines.toArray
-
   var matchCount = 0
   var validCharCount = 0
   var highCount=0
   var longestWord = ""
   var validWord = 0
 
-  for (whichWord <- 0 until wordAmount) {
+  for (whichWord <- 0 until stringsArray.length) {
     for (dictWordNum <- 0 until dictionary.length) {
       for (dictCharLocation <- 0 until dictionary(dictWordNum).length) {
         for(mycharLocation <- 0 until stringsArray(whichWord).length){
@@ -24,8 +28,8 @@ object Keyboard extends App {
         if(matchCount>dictCharLocation) validCharCount+=1
       }
       if (matchCount == dictionary(dictWordNum).length && matchCount>=highCount && validCharCount>=dictionary(dictWordNum).length){
-          highCount=matchCount
-          longestWord = dictionary(dictWordNum)
+        highCount=matchCount
+        longestWord = dictionary(dictWordNum)
       }
       matchCount=0
       validCharCount=0
